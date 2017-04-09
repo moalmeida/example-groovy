8d1638fb8ccdb3d4e6d7d6774dea15f9a61db7d4
moalmeida <moalmeida@koinosystems.com>
2017-04-09 18:29:19 -0300

    ...

--- api/app/src/main/groovy/example/RabbitConfiguration.groovy
+++ api/app/src/main/groovy/example/RabbitConfiguration.groovy
@@ -3,11 +3,6 @@ package example
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.core.Queue
-import org.springframework.amqp.core.TopicExchange
-import org.springframework.amqp.rabbit.annotation.Exchange
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -20,23 +15,11 @@ class RabbitConfiguration {
 
 	@Autowired
 	Environment env
-
-	@Bean String queueHost() {
-		env.getProperty("queue.host")
-	}
-
-	@Bean String queueUser() {
-		env.getProperty("queue.user")
-	}
-
-	@Bean String queuePwd() {
-		env.getProperty("queue.pwd")
-	}
-
+ 
 	@Bean String queueName() {
 		env.getProperty("queue.name")
 	}
-
+ 
 	@Bean int queueExpire() {
 		String expire = env.getProperty("queue.expire")
 		if (expire.isInteger()) {
@@ -44,20 +27,9 @@ class RabbitConfiguration {
 		}
 		return 0
 	}
-
-	@Bean ConnectionFactory connectionFactory() {
-		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(queueHost())
-		connectionFactory.setUsername(queueUser())
-		connectionFactory.setPassword(queuePwd())
-		return connectionFactory
-	}
-
-	@Bean RabbitTemplate rabbitTemplate() {
-		return new RabbitTemplate(connectionFactory())
-	}
-
+ 
 	@Bean Queue queue() {
-		return new Queue(queueName(), false)
+		return [queueName(), false] as Queue
 	}
 	
 }
--- api/app/src/test/groovy/example/RabbitConfigurationSpec.groovy
+++ api/app/src/test/groovy/example/RabbitConfigurationSpec.groovy
@@ -3,9 +3,6 @@ package example
 import org.springframework.core.env.Environment
 import org.springframework.test.context.ContextConfiguration
 
-import com.rabbitmq.client.ConnectionFactory
-
-import example.RabbitConfiguration
 import spock.lang.Specification
 
 @ContextConfiguration
@@ -13,44 +10,44 @@ class RabbitConfigurationSpec extends Specification {
 
 	RabbitConfiguration rabbitConfiguration
 	Environment env
-	ConnectionFactory connectionFactory
+	// ConnectionFactory connectionFactory
 
 	def setup() {
 		rabbitConfiguration = new RabbitConfiguration()
 	}
 
-	def "should queueHost() be loaded by Environment"() {
-		given: ""
-		env = Mock(Environment)
-		env.getProperty(_) >> "TESTE"
-		rabbitConfiguration.env = env
-		when: ""
-		def property = rabbitConfiguration.queueHost()
-		then: ""
-		property == "TESTE"
-	}
+	// def "should queueHost() be loaded by Environment"() {
+	// 	given: ""
+	// 	env = Mock(Environment)
+	// 	env.getProperty(_) >> "TESTE"
+	// 	rabbitConfiguration.env = env
+	// 	when: ""
+	// 	def property = rabbitConfiguration.queueHost()
+	// 	then: ""
+	// 	property == "TESTE"
+	// }
 
-	def "should queueUser() be loaded by Environment"() {
-		given: ""
-		env = Mock(Environment)
-		env.getProperty(_) >> "TESTE"
-		rabbitConfiguration.env = env
-		when: ""
-		def property = rabbitConfiguration.queueUser()
-		then: ""
-		property == "TESTE"
-	}
+	// def "should queueUser() be loaded by Environment"() {
+	// 	given: ""
+	// 	env = Mock(Environment)
+	// 	env.getProperty(_) >> "TESTE"
+	// 	rabbitConfiguration.env = env
+	// 	when: ""
+	// 	def property = rabbitConfiguration.queueUser()
+	// 	then: ""
+	// 	property == "TESTE"
+	// }
 
-	def "should queuePwd() be loaded by Environment"() {
-		given: ""
-		env = Mock(Environment)
-		env.getProperty(_) >> "TESTE"
-		rabbitConfiguration.env = env
-		when: ""
-		def property = rabbitConfiguration.queuePwd()
-		then: ""
-		property == "TESTE"
-	}
+	// def "should queuePwd() be loaded by Environment"() {
+	// 	given: ""
+	// 	env = Mock(Environment)
+	// 	env.getProperty(_) >> "TESTE"
+	// 	rabbitConfiguration.env = env
+	// 	when: ""
+	// 	def property = rabbitConfiguration.queuePwd()
+	// 	then: ""
+	// 	property == "TESTE"
+	// }
 
 	def "should queueName() be loaded by Environment"() {
 		given: ""
@@ -96,31 +93,21 @@ class RabbitConfigurationSpec extends Specification {
 		queue.name == "TESTE"
 	}
 
-	def "should connectionFactory() be loaded by Environment"() {
-		given: ""
-		env = Mock(Environment)
-		env.getProperty(_ as String) >> {  String message ->
-			message.equals("queue.host") ? "localhost" :
-					message.equals("queue.user") ? "user" :
-					message.equals("queue.pwd") ? "pwd" :
-					""
-		}
-		rabbitConfiguration.env = env
-		when: ""
-		def property = rabbitConfiguration.connectionFactory()
-		then: ""
-		property.host == "localhost"
-		property.username == "user"
-	}
+	// def "should connectionFactory() be loaded by Environment"() {
+	// 	given: ""
+	// 	env = Mock(Environment)
+	// 	env.getProperty(_ as String) >> {  String message ->
+	// 		message.equals("queue.host") ? "localhost" :
+	// 				message.equals("queue.user") ? "user" :
+	// 				message.equals("queue.pwd") ? "pwd" :
+	// 				""
+	// 	}
+	// 	rabbitConfiguration.env = env
+	// 	when: ""
+	// 	def property = rabbitConfiguration.connectionFactory()
+	// 	then: ""
+	// 	property.host == "localhost"
+	// 	property.username == "user"
+	// }
 
-	def "should rabbitTemplate() be loaded by ConnectionFactory"() {
-		given: ""
-		env = Mock(Environment)
-		env.getProperty(_) >> "TESTE"
-		rabbitConfiguration.env = env
-		when: ""
-		def queue = rabbitConfiguration.queue()
-		then: ""
-		queue.name == "TESTE"
-	}
 }
